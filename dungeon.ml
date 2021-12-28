a8ab62d90cc3c58604a7643dd275051fcdd29578
Alain Armand <idkjs@users.noreply.github.com>
2021-12-28 14:25:05 +0100

    update

--- dungeon.ml
+++ dungeon.ml
@@ -3,20 +3,20 @@ open Library
 open Sys
 
 let ()=
-  let parse_enemy (j:json)=match j with
-    | `Assoc obj -> let e=(new enemy) in begin List.iter (fun (a:string*json) -> match a with
+  let parse_enemy (j:t)=match j with
+    | `Assoc obj -> let e=(new enemy) in begin List.iter (fun (a:string*t) -> match a with
         | (k,`String v) when k="name" -> e#set_name v
         | (k,`Int i) when k="health" -> e#set_health i
         | (k,`Int i) when k="damage" -> e#set_dmg i
         | _ -> raise (Yojson.Json_error "Invalid JSON")
       ) obj;e end
     | _ -> raise (Yojson.Json_error "Invalid JSON") in
-  let rec parse_room (room:room) (j:json)= match j with
-    | `Assoc obj -> List.iter (fun (a:string * json) -> match a with
+  let rec parse_room (room:room) (j:t)= match j with
+    | `Assoc obj -> List.iter (fun (a:string * t) -> match a with
       | (k,`String v) when k="name" -> room#set_name v
-      | (k,`List v) when k="messages" -> List.iter (fun (a:json) -> match a with `String s -> room#add_msg s | _ -> raise (Yojson.Json_error "Invalid JSON")) v
-      | (k,`List v) when k="rooms" -> List.iter (fun (a:json) -> room#add_room (parse_room (new room) a)) v
-      | (k,`List v) when k="enemies" -> List.iter (fun (a:json) -> room#add_enemy (parse_enemy a)) v
+      | (k,`List v) when k="messages" -> List.iter (fun (a:t) -> match a with `String s -> room#add_msg s | _ -> raise (Yojson.Json_error "Invalid JSON")) v
+      | (k,`List v) when k="rooms" -> List.iter (fun (a:t) -> room#add_room (parse_room (new room) a)) v
+      | (k,`List v) when k="enemies" -> List.iter (fun (a:t) -> room#add_enemy (parse_enemy a)) v
       | _ -> raise (Yojson.Json_error "Invalid JSON")
     ) obj;room
     | _ -> raise (Yojson.Json_error "Invalid JSON") in
